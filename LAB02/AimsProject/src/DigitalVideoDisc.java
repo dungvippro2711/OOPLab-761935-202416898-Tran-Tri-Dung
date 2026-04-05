1505908145da4d532ebcc537c3a530aebfcd9527
dungvippro2711 <tridungtuki@gmail.com>
2026-04-05 21:22:18 +0700

    Hoan thanh Section 16: Tu dong tao ID cho DVD

--- LAB02/AimsProject/src/DigitalVideoDisc.java
+++ LAB02/AimsProject/src/DigitalVideoDisc.java
@@ -4,12 +4,18 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+    private int id;
+    private static int nbDigitalVideoDiscs = 0;
  
     public DigitalVideoDisc(String title) {
+        nbDigitalVideoDiscs++;      
+        this.id = nbDigitalVideoDiscs; 
         this.title = title;
     }
 
     public DigitalVideoDisc(String title, String category, float cost) {
+        nbDigitalVideoDiscs++;       
+        this.id = nbDigitalVideoDiscs;
         this.title = title;
         this.category = category;
         this.cost = cost;
@@ -23,13 +29,17 @@ public class DigitalVideoDisc {
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        nbDigitalVideoDiscs++;       
+        this.id = nbDigitalVideoDiscs;
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
     }
-
+    public int getId() {
+        return id;
+    }
     public String getTitle() { return title; }
     public String getCategory() { return category; }
     public String getDirector() { return director; }
