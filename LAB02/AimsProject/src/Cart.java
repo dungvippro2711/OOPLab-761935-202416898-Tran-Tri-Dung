2f1e5830e16b24da446ca82447b51352ba2c5d17
dungvippro2711 <tridungtuki@gmail.com>
2026-04-05 18:21:39 +0700

    Section 11, 12, 13

--- LAB02/AimsProject/src/Cart.java
+++ LAB02/AimsProject/src/Cart.java
@@ -2,6 +2,7 @@ public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered = 0;
+
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
         if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = disc;
@@ -11,10 +12,24 @@ public class Cart {
             System.out.println("The cart is almost full");
         }
     }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc... dvdList) {
+        for (DigitalVideoDisc disc : dvdList) {
+            if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+                itemsOrdered[qtyOrdered] = disc;
+                qtyOrdered++;
+                System.out.println("The disc has been added");
+            } else {
+                System.out.println("The cart is almost full");
+                break;
+            }
+        }
+    }
+
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         for (int i = 0; i < qtyOrdered; i++) {
             if (itemsOrdered[i] == disc) {
-                            for (int j = i; j < qtyOrdered - 1; j++) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
                     itemsOrdered[j] = itemsOrdered[j + 1];
                 }
                 itemsOrdered[qtyOrdered - 1] = null;
@@ -25,11 +40,16 @@ public class Cart {
         }
         System.out.println("The disc is not in the cart");
     }
+
     public float totalCost() {
         float total = 0;
         for (int i = 0; i < qtyOrdered; i++) {
             total += itemsOrdered[i].getCost();
         }
         return total;
+    }
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
     }
 }
