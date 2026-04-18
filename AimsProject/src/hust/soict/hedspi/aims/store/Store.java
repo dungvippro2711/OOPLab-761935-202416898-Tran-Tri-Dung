57d9b972dc0a9f76caee9ee9d9d5f6ece2ae7be2
dungvippro2711 <tridungtuki@gmail.com>
2026-04-18 20:03:03 +0700

    5

---
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -0,0 +1,33 @@
+package hust.soict.hedspi.aims.store;
+
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class Store {
+    private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[100]; 
+    private int qtyInStore = 0;
+
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (qtyInStore < itemsInStore.length) {
+            itemsInStore[qtyInStore] = dvd;
+            qtyInStore++;
+            System.out.println("The DVD " + dvd.getTitle() + " has been added to the store.");
+        } else {
+            System.out.println("The store is full!");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc dvd) {
+        for (int i = 0; i < qtyInStore; i++) {
+            if (itemsInStore[i] == dvd) {
+                for (int j = i; j < qtyInStore - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+                itemsInStore[qtyInStore - 1] = null;
+                qtyInStore--;
+                System.out.println("The DVD " + dvd.getTitle() + " has been removed from the store.");
+                return;
+            }
+        }
+        System.out.println("The DVD is not in the store.");
+    }
+}
