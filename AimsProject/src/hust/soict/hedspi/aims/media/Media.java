87d8ec01a9b53bb9661644d8bc71fd3535bbfb95
dungvippro2711 <tridungtuki@gmail.com>
2026-04-21 20:21:38 +0700

    17

--- AimsProject/src/hust/soict/hedspi/aims/media/Media.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/Media.java
@@ -1,11 +1,15 @@
 package hust.soict.hedspi.aims.media;
 
+import java.util.Comparator;
+
 public abstract class Media {
     private int id;
     private String title;
     private String category;
     private float cost;
     private static int nbMedia = 0;
+    public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
+    public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
 
     public Media(String title, String category, float cost) {
         this.title = title;
@@ -48,6 +52,6 @@ public abstract class Media {
 
     @Override
     public String toString() {
-        return "Media: " + title + " - Category: " + category + " - Cost: " + cost + "$";
+        return "Media [ID=" + id + ", Title=" + title + ", Category=" + category + ", Cost=" + cost + "$]";
     }
 }
