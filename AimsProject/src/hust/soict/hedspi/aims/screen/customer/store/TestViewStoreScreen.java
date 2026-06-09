91e4d1431748c4ff195de83c2968c2e6d1399b4a
dungvippro2711 <tridungtuki@gmail.com>
2026-06-09 22:13:36 +0700

    hoàn thành phần 5

---
+++ AimsProject/src/hust/soict/hedspi/aims/screen/customer/store/TestViewStoreScreen.java
@@ -0,0 +1,38 @@
+package hust.soict.hedspi.aims.screen.customer.store;
+
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.store.Store;
+import hust.soict.hedspi.aims.screen.customer.controller.ViewStoreController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestViewStoreScreen extends Application {
+    private static Store store;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final String STORE_FXML_FILE_PATH = "/hust/soict/hedspi/aims/screen/customer/view/Store.fxml";
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(STORE_FXML_FILE_PATH));
+        ViewStoreController viewStoreController = new ViewStoreController(store);
+        fxmlLoader.setController(viewStoreController);
+        Parent root = fxmlLoader.load();
+
+        primaryStage.setTitle("Store");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        store = new Store();
+        
+        // Add some items to store here
+        store.addMedia(new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f));
+        store.addMedia(new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f));
+        store.addMedia(new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99f));
+        
+        launch(args);
+    }
+}
